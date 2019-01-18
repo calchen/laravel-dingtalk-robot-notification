a7a68ee60aad539e56b1a688f79062d112657518
陈恺垣 <chenkaiyuan1993@gmail.com>
2019-01-18 10:55:17 +0800

    对配置项的默认环境变量名称进行修改

--- config/dingtalk_robot.php
+++ config/dingtalk_robot.php
@@ -4,9 +4,9 @@ return [
     // 默认发送的机器人
     'default' => [
         // 机器人的 access_token
-        'access_token' => env('DINGTALK_ROBOT_TOKEN', ''),
+        'access_token' => env('DINGTALK_ROBOT_ACCESS_TOKEN', ''),
 
         // 请求的超时时间
-        'timeout' => env('DINGTALK_ROBOT_TIME_OUT', 2.0)
+        'timeout' => env('DINGTALK_ROBOT_TIMEOUT', 2.0)
     ]
 ];
